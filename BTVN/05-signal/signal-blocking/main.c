c3ac19a0090edf1f0b1e6ef6f4ecabae77d8e448
dai.nd <dai.nd@lancsnet.com>
2023-07-27 13:18:45 +0700

    27/7/2023

--- BTVN/05-signal/signal-blocking/main.c
+++ BTVN/05-signal/signal-blocking/main.c
@@ -4,6 +4,40 @@
 #include <time.h>
 #include <unistd.h>
 
+void print_sigset_t(sigset_t *set)
+{
+    int i;
+
+    i = SIGRTMAX;
+    do
+    {
+        int x = 0;
+        i -= 4;
+        if (sigismember(set, i + 1))
+        {
+            printf("i + 1 = %d\n", i + 1);
+            x |= 1;
+        }
+        if (sigismember(set, i + 2))
+        {
+            printf("i + 2 = %d\n", i + 2);
+            x |= 2;
+        }
+        if (sigismember(set, i + 3))
+        {
+            printf("i + 3 = %d\n", i + 3);
+            x |= 4;
+        }
+        if (sigismember(set, i + 4))
+        {
+            printf("i + 4 = %d\n", i + 4);
+            x |= 8;
+        }
+        printf("%x\n", x);
+    } while (i >= 4);
+    printf("\n");
+}
+
 void sig_handler1(int signum)
 {
     printf("Im signal handler1\n");
